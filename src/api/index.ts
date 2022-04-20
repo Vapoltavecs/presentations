2087fda20a99f735a47240f292b0717c216cb9a7
Viacheslav <vapoltavecs@gmail.com>
2022-04-20 21:42:50 +0400

    fix url

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const $api = axios.create({
-  baseURL: "http://127.0.0.1:8000/",
+  baseURL: "http://87.249.49.88:8000/",
   headers: {
-    "Content-type": "application/json"
-  }
+    "Content-type": "application/json",
+  },
 });
 
 $api.interceptors.request.use((config) => {
